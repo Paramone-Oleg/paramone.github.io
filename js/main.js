89c6f3a308b479551568508479a602944c23d989
Oleg Ponamarchuk <49585728+Paramone-Oleg@users.noreply.github.com>
2019-04-22 20:11:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,18 +20,25 @@ $('.ajax-portfolio-link').click(function () {
 	return false;
 });
 
-(function () {
-  var scrollEl = document.querySelector('main')
-  var scr = new ScrollBooster({
-    viewport: document.querySelector('section#content'),
-    emulateScroll: true,
-    mode: 'x',
-    onUpdate: function (data) {
-      scrollEl.style.transform = 'translate(' + -data.position.x + 'px, ' + -data.position.y + 'px)'
+function handle_mousedown(e){
+    window.my_dragging = {};
+    my_dragging.pageX0 = e.pageX;
+    my_dragging.pageY0 = e.pageY;
+    my_dragging.elem = this;
+    my_dragging.offset0 = $(this).offset();
+    function handle_dragging(e){
+        var left = my_dragging.offset0.left + (e.pageX - my_dragging.pageX0);
+        var top = my_dragging.offset0.top + (e.pageY - my_dragging.pageY0);
+        $(my_dragging.elem)
+        .offset({top: top, left: left});
     }
-  })
-  scr.setPosition({
-    x: 100
-  })
-
-})()
+    function handle_mouseup(e){
+        $('body')
+        .off('mousemove', handle_dragging)
+        .off('mouseup', handle_mouseup);
+    }
+    $('body')
+    .on('mouseup', handle_mouseup)
+    .on('mousemove', handle_dragging);
+}
+$('#content').mousedown(handle_mousedown);

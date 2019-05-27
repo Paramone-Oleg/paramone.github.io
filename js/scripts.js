750eabb5014dd82b1c87a9fc753c0969a5c95020
Oleg Ponamarchuk <49585728+Paramone-Oleg@users.noreply.github.com>
2019-05-27 16:18:33 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -514,6 +514,17 @@ Function First Load
 			}
 		});
 		
+		$('.contact-link').on('click', function() {	
+			var acc = $("#main-page-content").height();			
+			if ($("body").hasClass("smooth-scroll")) {
+				TweenLite.to(scrollbar, 1.5, {scrollTop:acc, ease:Power4.easeInOut});
+			} else {
+				TweenLite.to(window, 1.5, {scrollTop:acc, ease:Power4.easeInOut});
+			}
+		});
+		
+		
+		
 		
 		// Tilt Showcase Wrapper
 		if( $('#hero').hasClass("has-image")) {				
